3dca5dff97e5aa7ed8f498d9095b8d2c021f88aa
Vinit Suryarao <suryarao.vinit@hotmail.com>
2020-11-27 12:18:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using Statement set boundary of object which outside that object will automatically destroy.
+            using (StreamReader reader = File.OpenText(@"E:\Code\C#\Using_Statement\TempFile.txt"))
+            {
+                if (reader.EndOfStream)
+                    return;
+                Console.WriteLine(reader.ReadToEnd());
+            }
+            Console.ReadLine();
+        }
+    }
+}
